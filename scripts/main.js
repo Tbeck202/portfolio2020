05abafbdd98936a67ab0025ab5170799f8d6ea13
Tyler Beck <tbeck202@gmail.com>
2019-12-26 11:45:13 -0800

    add avatar ID

--- scripts/main.js
+++ scripts/main.js
@@ -3,6 +3,21 @@ $('.ui.dropdown')
       on: 'hover'
   });
 
+$('#avatar')
+  .transition('horizontal flip in', '2000ms')
+;
+
+// $('.tiny')
+//   .transition({
+//     animation : 'zoom in',
+//     interval  : 200
+//   })
+// ;
+
+$('.tiny')
+  .transition('horizontal flip in', '2000ms')
+;
+
 
 
 
